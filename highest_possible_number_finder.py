f96430b024d81a18e42ec643633607286315e65d
Marcus Guerrero <marcus.guerrero61@gmail.com>
2024-10-17 16:28:58 +0800

    Adding the pseodocode

---
+++ highest_possible_number_finder.py
@@ -0,0 +1,3 @@
+#create a unique function for the if statements that would be used for locating the highest possible number out of five random values
+#make five input lines that will ask users to input any random number
+#state the unique function name to process the command in the terminal
